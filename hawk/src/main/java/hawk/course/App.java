fe1098644ac1063d05ec6f3eeea7c8bdc5649706
shahinsiami <shahinsiami@hotmail.com>
2022-06-10 22:06:35 +0430

    session 10

--- hawk/src/main/java/hawk/course/App.java
+++ hawk/src/main/java/hawk/course/App.java
@@ -7,7 +7,7 @@ public final class App {
     }
 
     public static void main(String[] args) {
-  
+        
     }
     
 }
---
+++ hawk/src/main/java/hawk/course/session10_class.java
@@ -0,0 +1,10 @@
+// package hawk.course;
+
+// public class session9_class {
+//     public static void printStaticObject(){
+//         System.out.println("its a static method");
+//     }
+//  }
+
+
+ 
